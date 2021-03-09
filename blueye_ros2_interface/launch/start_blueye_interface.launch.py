b8959d8be449a330e9328840abfc578b7c99b3b5
nadir-kapetanovic <nadir.kapetanovic@fer.hr>
2021-03-09 17:28:46 +0100

    Added rqt_image_view node to the launch file. Added generic rqt_gui node to the launch file with a --perspective-file argument. Added *.perspective files to data_files in setup.py.

--- blueye_ros2_interface/launch/start_blueye_interface.launch.py
+++ blueye_ros2_interface/launch/start_blueye_interface.launch.py
@@ -59,10 +59,29 @@ def generate_launch_description():
             #remappings= []
         )
     
+    rqt_gui_node_perspective =  os.path.join(
+        get_package_share_directory('blueye_ros2_interface'),
+        'config',
+        'blueye_rqt_gui.perspective'
+        )  
+    
+    rqt_gui_node = Node (
+            namespace='blueye',
+            name='gui',
+            package='rqt_gui',
+            executable='rqt_gui',
+            output='screen',
+            emulate_tty=True,
+            arguments = [ '--perspective-file', rqt_gui_node_perspective  ] 
+            #parameters= [],
+            #remappings= []
+        )
+    
     
     ld.add_action(blueye_node)
     ld.add_action(gstreamer_node)
-    ld.add_action(rqt_image_view_node)
+    #ld.add_action(rqt_image_view_node)
+    ld.add_action(rqt_gui_node)
     
     return ld
 
