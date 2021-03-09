ea841a02c69b5d5174ecce8c1572b8ebef001855
nadir-kapetanovic <nadir.kapetanovic@fer.hr>
2021-03-09 14:38:52 +0100

    Added gscam2 and ros2_shared submodules. Added gstreamer_node with params in start_blueye...launch.py.

--- blueye_ros2_interface/launch/start_blueye_interface.launch.py
+++ blueye_ros2_interface/launch/start_blueye_interface.launch.py
@@ -12,13 +12,13 @@ def generate_launch_description():
     blueye_params = os.path.join(
         get_package_share_directory('blueye_ros2_interface'),
         'config',
-        'blueye_ros2_params.yaml'
+        'ros2_interface_node_params.yaml'
         )
     
     
     blueye_node =  Node(
-			namespace='blueye',
-			name='blueye_interface',
+            namespace='blueye',
+            name='ros2_interface_node',
             package='blueye_ros2_interface',            
             executable='blueye_interface',            
             output='screen',
@@ -26,8 +26,43 @@ def generate_launch_description():
             parameters= [blueye_params],
             remappings=[]
         )
+        
+    gstreamer_node_params = os.path.join(
+        get_package_share_directory('blueye_ros2_interface'),
+        'config',
+        'gstreamer_node_params.yaml'
+        )
+        
+    gstreamer_remappings = [('image_raw', 'camera_img_raw'),
+                  ('camera_info', 'camera_calibration_params')]
+    
+    gstreamer_node = Node (
+            namespace='blueye',
+            name='gstreamer_node',
+            package='gscam',
+            executable='gscam_main',
+            output='screen',
+            emulate_tty=True,
+            parameters= [gstreamer_node_params],
+            remappings=gstreamer_remappings
+    
+        )
+    
+    rqt_image_view_node = Node (
+            namespace='blueye',
+            name='rqt_image_view_node',
+            package='rqt_image_view',
+            executable='rqt_image_view',
+            output='screen',
+            emulate_tty=True,
+            #parameters= [],
+            #remappings= []
+        )
+    
     
     ld.add_action(blueye_node)
+    ld.add_action(gstreamer_node)
+    ld.add_action(rqt_image_view_node)
     
     return ld
 
